52888b964931275c813787cd04e3e870852ff8bc
Jeremy Schaub <schaubjeremy@gmail.com>
2024-10-01 15:28:23 -0600

    Leptonica--reduced layer size; compile on EC2 x86_64

--- leptonica/README.md
+++ leptonica/README.md
@@ -3,19 +3,11 @@
 [Leptonica](http://www.leptonica.org/) is a open source library containing software that
 is broadly useful for image processing and image analysis applications.
 
-The lambda layer is [layer/leptonica-layer.zip](layer/leptonica-layer.zip).
+The lambda layer(s) are in [layers/](layers/).
 - v1.8.2
-- arm64 architecture
 
 ## Build it yourself
-Create the docker image
-```bash
-docker build -t amazonlinux-leptonica .
-```
+Create an EC2 instance with an Amazon Linux AMI
 
-Run the container:
-```bash
-docker run -it amazonlinux-leptonica  # the "SRC" container (see script)
-```
-
-Edit and run the [`create_layer.sh`](create_layer.sh) script.
+Run the [`install_leptonica.sh`](install_leptonica.sh) script.
+Run the [`create_layer.sh`](create_layer.sh) script.
--- leptonica/create_layer.sh
+++ leptonica/create_layer.sh
@@ -9,31 +9,29 @@ mkdir -p layer/lib64
 mkdir -p layer/lib
 mkdir -p layer/bin
 
-SRC=5d0ba7366eb2  # docker container
-
-for binfn in convertfilestopdf  convertfilestops  convertformat  convertsegfilestopdf  convertsegfilestops  converttopdf  converttops  fileinfo  imagetops  xtractprotos; do
-	docker cp "$SRC:/usr/local/bin/$binfn" layer/bin
+for binfn in convertfilestopdf convertfilestops convertformat convertsegfilestopdf convertsegfilestops converttopdf converttops fileinfo imagetops xtractprotos; do
+	cp "/usr/local/bin/$binfn" layer/bin
 done
 
 # Copy the library files loaded at runtime
-docker exec "$SRC" ldd /usr/local/bin/converttopdf | grep lib64 | perl -pe "s/^\s(.*) =\>.*/\1/" >lib64_files.txt
-docker exec "$SRC" ldd /usr/local/bin/converttopdf | grep "/usr/local" | perl -pe "s/^\s(.*) =\>.*/\1/" >lib_files.txt
+ldd /usr/local/bin/converttopdf | grep "=> /lib64" |
+	perl -pe "s/^\s(.*) =\>.*/\1/" >lib64_files.txt
+ldd /usr/local/bin/converttopdf | grep "/usr/local" |
+	perl -pe "s/^\s(.*) =\>.*/\1/" >lib_files.txt
 
 while IFS= read -r filename; do
-	echo "Copying $filename to layer/lib64"
-	docker cp "$SRC:/usr/lib64/$filename" layer/lib64/
-	linked_file=$(docker exec "$SRC" readlink -f "/usr/lib64/$filename")
-	echo "Copying $linked_file to layer/lib64"
-	docker cp "$SRC:$linked_file" layer/lib64/
+	linked_file=$(readlink -f "/usr/lib64/$filename")
+	echo "Copying $linked_file to layer/lib64/$filename"
+	cp "$linked_file" "layer/lib64/$filename"
 done <lib64_files.txt
 
 while IFS= read -r filename; do
-	echo "Copying $filename to layer/lib"
-	docker cp "$SRC:/usr/local/lib/$filename" layer/lib/
-	linked_file=$(docker exec "$SRC" readlink -f "/usr/local/lib/$filename")
-	echo "Copying $linked_file to layer/lib"
-	docker cp "$SRC:$linked_file" layer/lib/
+	linked_file=$(readlink -f "/usr/local/lib/$filename")
+	echo "Copying $linked_file to layer/lib/$filename"
+	cp "$linked_file" "layer/lib/$filename"
 done <lib_files.txt
+
+cp -r img layer/test
 
 cd layer || exit
 zip -r9 leptonica-layer.zip .
